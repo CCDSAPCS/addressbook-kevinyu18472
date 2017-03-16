a6185b171669a064c66ce88ad7b3097bc775fdf5
kevinyu18472 <yuk@countryday.net>
2017-03-16 09:41:30 -0400

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -23,5 +23,61 @@ public class AddressBook {
 		return numfriends;
 	}
 	public int haveContact(String s){
-		//iterate through the list of friends i = 0 to numfriends
-		//if(friends[i].getName.equals(s)
+		for (int i = 0; i<numfriends; i++)
+		{
+			if(friends[i].getName().equals(s)
+			   {
+				   return i;
+			   }
+		}
+		return -1;
+	}
+	public int delete(String s){
+		for (int i = 0; i<numfriends; i++)
+		{
+			if(friends[i].getName().equals(s)
+			   {
+				   return i;
+				   int temp = friends[friends.length-1];
+				   friends[friends.length-1] = friends[1];
+				   friends[i] = temp;
+				   numfriends --;
+			   }
+		}
+	public static void main(String[] args){
+		Scanner scan = new Scanner(system.in);
+		AddressBook syrup = new AddressBook()
+		syrup.menu();
+		int choice = scan.nextInt();
+		if(choice == 1) {
+			syrup.addContact();
+		}
+		if(choice == 2) {
+			syrup.delete();
+		}
+		if(choice == 3) {
+			System.out.println("You have " + syrup.numContacts() + " friends");
+		}
+		if(choice == 4) {
+			syrup.printContacts();	
+		}
+		if(choice == 5) {
+		}
+	}
+			   
+	public static void menu(){
+		System.out.println("1. Add a contact");
+		System.out.println("2. Delete a contact");
+		System.out.println("3. Print the number of contacts");
+		System.out.println("4. Print all contacts");
+		System.out.println("5. Quit");
+		System.out.println("Enter your choice: ");
+	}
+
+
+
+
+
+		
+			   
+		
